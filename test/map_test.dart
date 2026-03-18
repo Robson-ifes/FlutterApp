b62064fa3ad695a5621e4d0e7547e4c1f1eb825f
robinho.ufes <robinho.ufes@gmail.com>
2026-03-18 23:29:27 +0000

    Semana 4

---
+++ test/map_test.dart
@@ -0,0 +1,100 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  late Map<String, List<double>> alunos;
+
+  setUp(() {
+    alunos = {
+      'Maria': [8.0, 9.0],
+      'Bruna': [7.0, 7.0],
+      'Carla': [10.0, 9.0],
+    };
+  });
+
+  test('Adicionar elemento', () {
+    alunos.putIfAbsent('Elena', () => [9.0, 8.0]);
+
+    expect(alunos.containsKey('Elena'), isTrue);
+    expect(alunos['Elena'], [9.0, 8.0]);
+  });
+
+  test('Adicionar outro dicionário', () {
+    alunos.addAll({
+      'Elena': [9.0, 8.0],
+      'Luiza': [8.0, 9.0],
+    });
+
+    expect(alunos.containsKey('Elena'), isTrue);
+    expect(alunos.containsKey('Luiza'), isTrue);
+  });
+
+  test('Remover elemento', () {
+    alunos.remove('Bruna');
+
+    expect(alunos.containsKey('Bruna'), isFalse);
+  });
+
+  test('Atualizar elemento', () {
+    alunos.update('Carla', (value) => [9.0, 8.0]);
+    expect(alunos['Carla'], [9.0, 8.0]);
+
+    alunos['Carla'] = [8.0, 9.0];
+    expect(alunos['Carla'], [8.0, 9.0]);
+  });
+
+  test('Testar percorrer dicionário', () {
+    alunos.addAll({
+      'Elena': [9.0, 8.0],
+      'Luiza': [8.0, 9.0],
+    });
+    alunos.remove('Bruna');
+    alunos['Carla'] = [8.0, 9.0];
+
+    expect(alunos.keys, ['Maria', 'Carla', 'Elena', 'Luiza']);
+    expect(alunos.values, [
+      [8.0, 9.0],
+      [8.0, 9.0],
+      [9.0, 8.0],
+      [8.0, 9.0],
+    ]);
+
+    double soma = 0;
+    alunos.forEach((key, value) {
+      for (double nota in value) {
+        soma += nota;
+      }
+    });
+
+    expect(soma, 68.0);
+  });
+
+  test('Calcular médias', () {
+    Map<String, List<double>> alunos = {
+      'Maria': [8.0, 9.0],
+      'Carla': [8.0, 9.0],
+      'Elena': [9.0, 8.0],
+      'Luiza': [8.0, 9.0],
+    };
+
+    Map<String, double> medias = {};
+
+    alunos.forEach((nome, notas) {
+      double soma = 0;
+
+      for (double nota in notas) {
+        soma += nota;
+      }
+
+      double media = soma / notas.length;
+
+      medias[nome] = media;
+    });
+
+    expect(medias, {
+      'Maria': 8.5,
+      'Carla': 8.5,
+      'Elena': 8.5,
+      'Luiza': 8.5
+    });
+  });
+}
